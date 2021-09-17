eabdaa9d82b57c9420fa479f09e20a29421cb479
Juan-garcia19 <jesteban.garcia1@udea.edu.co>
2021-09-16 22:28:59 -0500

    para esta parte se termino la funcion o el algoritmo para actualizar la tabla de enrutadores utilizando un vector aninado en un mapa

--- main.cpp
+++ main.cpp
@@ -1,65 +1,105 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <map>
 #include <string>
-
 using namespace std;
 
 
-void lecturaCaminos(char router, vector<string> NomConexRuter,vector<int> valConexRuter, string caminoRecorrido, char destino,char copi,char an,int ValorCami,int &B);
+void lecturaCaminos(char router, vector<string> NomConexRuter,vector<int> valConexRuter, string caminoRecorrido, char destino,char copi,char an,int ValorCami,int &B,string &caminoMayor);
 string LectuArchi(vector<string> &NomConexRuter,vector<int> &valConexRuter);
 
 int main()
 {
-    //vector <int> valConexRuter ={4,1,10,4,3,1,10,2,1,2,1,3};
-    //vector <string> NomConexRuter = {"AB","AE","AC","BA","BE","BD","CA","CD","DB","DC","EA","EB"};
-    vector <int> valConexRuter = {} ;
-    vector <string> NomConexRuter ={} ;
+    vector <int> valConexRuter ={4,10,4,3,1,10,2,1,2,1,3,1};
+    vector <string> NomConexRuter = {"AB","AC","BA","BE","BD","CA","CD","DB","DC","EA","EB","AE"};
+    string caminoMayor;
+    //vector <int> valConexRuter = {} ;
+    //vector <string> NomConexRuter ={} ;
 
-    int ValorCami=0;
-    int B=0;
+    //int ValorCami=0;
+    //int B=100;
 
 
+    map <char,vector<string>> routers={};
+
+    vector <char> Ruter={};
+
+
+
+
+
+
+    //LectuArchi(NomConexRuter,valConexRuter);
+
+    //char router='C',an='C';
+    //char destino='D',copi=router;
     string caminoRecorrido;
 
-    LectuArchi(NomConexRuter,valConexRuter);
+    //lecturaCaminos(router,NomConexRuter,valConexRuter,caminoRecorrido,destino,copi,an,ValorCami,B,caminoMayor);
 
-    char router='A',an=NomConexRuter[0][0];
-    char destino='D',copi=router;
+    for (auto router:NomConexRuter){
+        routers[router[0]]={};
+    }
 
-  //  lecturaCaminos(router,NomConexRuter,valConexRuter,caminoRecorrido,destino,copi,an,ValorCami,B);
+    for (auto DefinicionR:routers){
+        for (auto parametros:routers){
+
+            char copi=DefinicionR.first, an=DefinicionR.first;
+            int ValorCami=0;
+            int B=100;
+            string caminoRecorrido,claveInter,Valor;
+            string caminoMayor;
+            claveInter.push_back(parametros.first);
+
+            lecturaCaminos(DefinicionR.first,NomConexRuter,valConexRuter,caminoRecorrido,parametros.first,copi,an,ValorCami,B,caminoMayor);
+            routers[DefinicionR.first].push_back(claveInter);
+            routers[DefinicionR.first].push_back(to_string(B));
+            routers[DefinicionR.first].push_back(caminoMayor);
 
 
-
-
+        }
+    }
+    routers['A'].push_back("B");
+    routers['A'].push_back("7");
 
     return 0;
 }
 
 
 
-void lecturaCaminos(char router, vector<string> NomConexRuter,vector<int> valConexRuter, string caminoRecorrido, char destino, char copi,char an,int ValorCami,int &B){
+void lecturaCaminos(char router, vector<string> NomConexRuter,vector<int> valConexRuter, string caminoRecorrido, char destino, char copi,char an,int ValorCami,int &B,string &caminoMayor){
     //copi=router;
     int longitud=NomConexRuter.size();
 
-    string caminoMayor;
+    //caminoMayor="";
 
     for(int i=0;i<longitud;i++){
         if ( NomConexRuter[i][0] == copi){
+            int cont=0;
+            for (auto letra:caminoRecorrido){
+                if(NomConexRuter[i][1]==letra){
+                    cont++;
+                }
+            }
+            if (cont>1){
 
-            if (NomConexRuter[i][1]==router or an==NomConexRuter[i][1]){
-                //cout<<"Camino no valido"<<endl;
-                //caminoRecorrido="";
+            }
+
+            else if (NomConexRuter[i][1]==router or an==NomConexRuter[i][1]){
+
             }
             else if(NomConexRuter[i][1]==destino){
                 caminoRecorrido+=NomConexRuter[i];
                 ValorCami+=valConexRuter[i];
-                //cout<<"el camino es: "<<caminoRecorrido<<" y el valor es "<<ValorCami<<endl;
 
-                if(ValorCami>B){
+                if(ValorCami<B){
                     B=ValorCami;
                     caminoMayor=caminoRecorrido;
-                    cout<<"el camino es: "<<caminoRecorrido<<" y el valor es "<<ValorCami<<endl;
+                    cout<<"el camino es: "<<caminoMayor<<" y el valor es "<<B<<endl;
+                    ValorCami-=valConexRuter[i];
+                    caminoRecorrido.pop_back();
+                    caminoRecorrido.pop_back();
                 }
 
 
@@ -71,7 +111,7 @@ void lecturaCaminos(char router, vector<string> NomConexRuter,vector<int> valCon
 
                 copi=NomConexRuter[i][1];
                 an=NomConexRuter[i][0];
-                lecturaCaminos(router,NomConexRuter,valConexRuter,caminoRecorrido,destino,copi,an,ValorCami,B);
+                lecturaCaminos(router,NomConexRuter,valConexRuter,caminoRecorrido,destino,copi,an,ValorCami,B,caminoMayor);
                 copi=NomConexRuter[i][0];
 
                 ValorCami-=valConexRuter[i];
@@ -97,13 +137,9 @@ string LectuArchi(vector<string> &NomConexRuter, vector<int> &valConexRuter){
      cout << "Error abriendo el archivo" << endl;
      exit(1);
    }
-
-   cout << "Leyendo el archivo" << endl;
    infile >> data;
 
-
    string linea;
-
 
    while(!infile.eof()){
        string conexionRuter,ValorConex;
